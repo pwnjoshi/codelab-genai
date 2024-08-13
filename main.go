4d2592d30bd17c4d1d2f72e31aaf5fe64c4d82a3
Pawan Joshi <joshipawan2021@gmail.com>
2024-08-13 18:56:35 +0000

    add latest changes

--- main.go
+++ main.go
@@ -1,22 +1,63 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/compute/metadata"
+	"cloud.google.com/go/vertexai/genai"
 )
 
 func main() {
+	ctx := context.Background()
+	var projectId string
+	var err error
+	projectId = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectId == "" {
+		projectId, err = metadata.ProjectIDWithContext(ctx)
+		if err != nil {
+			return
+		}
+	}
+	var client *genai.Client
+	client, err = genai.NewClient(ctx, projectId, "us-central1")
+	if err != nil {
+		return
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash-001")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+		animal := r.URL.Query().Get("animal")
+		if animal == "" {
+			animal = "dog"
+		}
+
+		resp, err := model.GenerateContent(
+			ctx,
+			genai.Text(
+				fmt.Sprintf("Give me 10 fun facts about %s. Return the results as HTML without markdown backticks.", animal)),
+		)
+
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+
+		if len(resp.Candidates) > 0 && len(resp.Candidates[0].Content.Parts) > 0 {
+			htmlContent := resp.Candidates[0].Content.Parts[0]
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprint(w, htmlContent)
+		}
 	})
 
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = "8080"
 	}
-	if err := git config --global user.name "${GITHUB_USERNAME}"http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(":"+port, nil)
 }
